3906741ee7c805662f608655c54719dc30f42300
LoiseauB <bapt.loiseau@gmail.com>
2022-07-11 17:15:42 +0200

    FINI

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,14 +1,23 @@
 def translate str
+  arr = str.split
   alpha = ('a'..'z').to_a
   vowels = %w[a e i o]
   consonants = alpha - vowels
-  if vowels.include?(str[0])
-    str + 'ay'
-  elsif consonants.include?(str[0]) && consonants.include?(str[1]) && consonants.include?(str[2])
-    str[3..-1] + str[0..2] + 'ay'
-  elsif consonants.include?(str[0]) && consonants.include?(str[1])
-    str[2..-1] + str[0..1] + 'ay'
-  elsif consonants.include?(str[0])
-    str[1..-1] + str[0] + 'ay'
+  result =[]
+  arr.each do |word|
+    if vowels.include?(word[0])
+      new = word + 'ay'
+      result.push(new)
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      new = word[3..-1] + word[0..2] + 'ay'
+      result.push(new)
+    elsif consonants.include?(word[0]) && consonants.include?(word[1])
+      new  = word[2..-1] + word[0..1] + 'ay'
+      result.push(new)
+    elsif consonants.include?(word[0])
+      new = word[1..-1] + word[0] + 'ay'
+      result.push(new)
+    end
   end
+  return result.join(' ')
 end
